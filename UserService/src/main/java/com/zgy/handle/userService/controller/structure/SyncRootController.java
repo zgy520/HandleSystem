73c9f17ac60da576fada4c59f1a83c47cc67b209
a4423 <a442391947@gmail.com>
2020-06-08 06:52:28 +0800

    企业节点上报

--- UserService/src/main/java/com/zgy/handle/userService/controller/structure/SyncRootController.java
+++ UserService/src/main/java/com/zgy/handle/userService/controller/structure/SyncRootController.java
@@ -4,6 +4,7 @@ import com.zgy.handle.userService.service.structure.SyncEnterpriseToRootService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class SyncRootController {
     private SyncEnterpriseToRootService syncEnterpriseToRootService;
 
     @PostMapping(value = "enterprise")
-    public String syncRoot(String enterpriseInfo){
+    public String syncRoot(@RequestBody String enterpriseInfo){
         log.info(enterpriseInfo);
         return "success";
     }
