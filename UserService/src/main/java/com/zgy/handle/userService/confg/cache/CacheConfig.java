dd4960cfa9384d2b3e1517943ccc010ae30bc82d
a4423 <a442391947@gmail.com>
2020-08-17 20:16:20 +0800

    缓存

--- UserService/src/main/java/com/zgy/handle/userService/confg/cache/CacheConfig.java
+++ UserService/src/main/java/com/zgy/handle/userService/confg/cache/CacheConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableCaching
+//@EnableCaching
 public class CacheConfig {
     /*@Bean
     public CacheManager cacheManager(){
